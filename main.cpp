9fd1b973a28e873f15d2fc442da3071a01435817
Paul <paulcoignac@gmail.com>
2026-04-22 21:06:38 +0200

    Add main.cpp to simplify code testing in development

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+
+int main() {
+    std::cout << "Subvision CV - Development Play Ground" << std::endl;
+    // CODE TO TEST
+
+    std::cout << "Hello Diver !" << std::endl;
+
+    // END OF CODE TO TEST
+    return 0;
+}
+
